b70f881bcf02ab7666b816f5d4ec127fd5caaf24
arcpetretta <arcpetretta@gmail.com>
2017-07-05 12:45:51 -0400

    updating NuGet Packages; app now serves static index.html page by default

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+
 
 namespace TheWorld
 {
@@ -19,19 +13,10 @@ namespace TheWorld
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("<html><body><h3>Hello World!</h3><body></html>");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
