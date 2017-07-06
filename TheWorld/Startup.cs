1d804c7311372fcc00f4e5bb207bc4b52bcc4be1
arcpetretta <arcpetretta@gmail.com>
2017-07-06 12:21:31 -0400

    Web Server middleware now loads serves correct view via startup.cs

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-
 
 namespace TheWorld
 {
@@ -10,12 +10,21 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
+            app.UseMvc(config => 
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                );
+            });
         }
     }
 }
