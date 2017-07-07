e7aa56649feb2f0535a93d2ea8a59af375c2d64b
arcpetretta <arcpetretta@gmail.com>
2017-07-07 16:37:14 -0400

    Load in Bootstrap, font-awesome and bootswatch themes via Bower; Apply themes and styling

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,17 +1,17 @@
 ﻿// site.js
 
 (function () {
-    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
 
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
-
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
-        }
-        else
-        {
-            $(this).text("Hide Sidebar");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
     });
 })();
