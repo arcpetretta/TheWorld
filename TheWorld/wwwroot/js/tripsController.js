46e4c1c4061f5aa0cce8aac72b4ff44eb435c2dd
arcpetretta <arcpetretta@gmail.com>
2017-08-08 16:41:09 -0400

    First Angular controller, basic databinding, handling forms with framework

---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,28 @@
+﻿// tripsController.js
+(function () {
+    "use strict";
+
+    // gets existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        var vm = this;
+
+        vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+                name: "WorldTrip",
+                created: new Date()
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            alert(vm.newTrip.name);
+        }
+    }
+
+})();
