2dff48bdcf8cb1a3bd9b1641382f70573954979d
arcpetretta <arcpetretta@gmail.com>
2017-08-08 17:30:11 -0400

    retrieving data from api with angular

--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -6,19 +6,28 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        }, {
-                name: "WorldTrip",
-                created: new Date()
-        }];
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                //SUCCESS
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                //FAIL
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
